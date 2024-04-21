7788cef57b0f52a29eba61d7cab7f4bfab3f54d9
mrzahidfaiz <mrzahidfaiz@gmail.com>
2024-04-21 20:20:27 +0500

    now Submission works

--- express-server/src/index.ts
+++ express-server/src/index.ts
@@ -6,6 +6,27 @@ app.use(express.json());
 
 const client = createClient();
 
+client.on("error", (error) => console.log(`Error on Client Redis ${error}`));
+
+app.get("/", (req, res) => {
+  res.send("Hello From Server!");
+});
+
+app.post("/send", async (req, res) => {
+  const { problemId, userId, code, language } = req.body;
+  try {
+    await client.lPush(
+      "problems",
+      JSON.stringify({ problemId, userId, code, language })
+    );
+
+    res.status(200).send("Submission received and stored.");
+  } catch (error) {
+    console.log(`Redis Error ${error}`);
+    res.status(500).send("Failed to store submission.");
+  }
+});
+
 async function startServer() {
   try {
     await client.connect();
